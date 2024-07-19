ee24bb7c3182440747546333571bf4602142b188
david.hamoui <t.david.hamoui@fabrica.locaweb.com.br>
2024-07-19 13:00:19 -0300

    adding 'days' parameter

--- gluejob.py
+++ gluejob.py
@@ -19,9 +19,8 @@ def convert_to_partitions(s3_objects):
 
     check_for()
 
-    logger.info(f"_______ {env['job_mode']}ing ALL OBJECTS IN {env['table_name']} _______")
+    logger.info(f"_______ PARTITIONING ALL OBJECTS IN {env['table_name']} TO {env['job_mode']} _______")
 
-    all_dfs = []
     joined_df = pd.DataFrame()
     
     for item in s3_objects:
@@ -32,15 +31,15 @@ def convert_to_partitions(s3_objects):
         data_frame = pd.read_parquet(body)
         data_frame['created_at'] = pd.to_datetime(data_frame['created_at'])
         
-        joined_df = pd.concat([joined_df,data_frame],ignore_index=True)
+        joined_df = pd.concat(objs=[joined_df,data_frame],ignore_index=True)
     
     joined_df = joined_df.sort_values(by='created_at')
 
-    grouped = joined_df.groupby(by=pd.Grouper(key='created_at',freq=parameter_dict[env['job_mode']][0].upper()))
+    grouped = joined_df.groupby(by=pd.Grouper(key='created_at',freq=env['job_mode'][0].upper()))
 
     list_of_all_partitions = []
     for date,df in grouped:
-        list_of_all_partitions.append([getattr(date,parameter_dict[env['job_mode']]),df.copy()])
+        list_of_all_partitions.append([date,df.copy()])
 
     return list_of_all_partitions
 
@@ -89,7 +88,10 @@ def upload_partitions_to_s3(list_of_all_partitions):
     for date,df in list_of_all_partitions:
         parquet_bytes = BytesIO(df.to_parquet())
 
-        s3.upload_fileobj(Fileobj=parquet_bytes,Bucket=env['bucket'], Key = f"{env['path']}/{env['table_name']}/company=Locaweb/new-folder/{env['table_name']}_{parameter_dict[env['job_mode']]}={date}/{env['table_name']}.parquet")
+        if env['job_mode'] == 'Years':
+            s3.upload_fileobj(Fileobj=parquet_bytes, Bucket=env['bucket'], Key = f"{env['path']}/{env['table_name']}/company=Locaweb/new-folder/{env['table_name']}_{env['job_mode'].lower()[:-1]}={date.year}/{env['table_name']}.parquet")
+        elif env['job_mode'] == 'Days':
+            s3.upload_fileobj(Fileobj=parquet_bytes, Bucket=env['bucket'], Key = f"{env['path']}/{env['table_name']}/company=Locaweb/new-folder/{env['table_name']}_year={date.year}/{env['table_name']}_month={date.month}/{env['job_mode'].lower()[:-1]}={date.day}/{env['table_name']}.parquet")
 
 
     '''
@@ -122,13 +124,7 @@ logger.info(f"***INITIALIZING GLUEJOB SCRIPT***     Parameters: Bucket = {env['b
 s3 = boto3.client('s3')
 
 
-parameter_dict = {
-    "Shrink": 'year',
-    "Expand": 'month'
-}
-
 objects = read_all_objects_in_path()
-
 
 partitioned_dfs = convert_to_partitions(objects)
 upload_partitions_to_s3(partitioned_dfs)
