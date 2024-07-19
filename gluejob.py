13aaba86f7a3548ad616855e95ec3573e0b2d96b
david.hamoui <t.david.hamoui@fabrica.locaweb.com.br>
2024-07-19 11:44:09 -0300

    'Shrink' job_mode updated and tested

--- gluejob.py
+++ gluejob.py
@@ -10,7 +10,7 @@ from io import BytesIO
 
 def read_all_objects_in_path():
     logger.info(f"_______ READING ALL FILES FROM BUCKET {env['bucket']}, FOLDER {env['path']}, TABLE {env['table_name']} _______")
-    response = s3.list_objects_v2(Bucket=env['bucket'],Prefix=f"{env['path']}{env['table_name']}")
+    response = s3.list_objects_v2(Bucket=env['bucket'],Prefix=f"{env['path']}/{env['table_name']}/")
     s3_objects = response['Contents']
 
     return s3_objects
@@ -22,6 +22,7 @@ def convert_to_partitions(s3_objects):
     logger.info(f"_______ {env['job_mode']}ing ALL OBJECTS IN {env['table_name']} _______")
 
     all_dfs = []
+    joined_df = pd.DataFrame()
     
     for item in s3_objects:
         # verifique o nivel máximo atual de particionamento e se esse e igual ao pedido
@@ -31,7 +32,20 @@ def convert_to_partitions(s3_objects):
         data_frame = pd.read_parquet(body)
         data_frame['created_at'] = pd.to_datetime(data_frame['created_at'])
         
-        grouped_df = data_frame.groupby(pd.Grouper(key='created_at', freq='D'))
+        joined_df = pd.concat([joined_df,data_frame],ignore_index=True)
+    
+    joined_df = joined_df.sort_values(by='created_at')
+
+    grouped = joined_df.groupby(by=pd.Grouper(key='created_at',freq=parameter_dict[env['job_mode']][0].upper()))
+
+    list_of_all_partitions = []
+    for date,df in grouped:
+        list_of_all_partitions.append([getattr(date,parameter_dict[env['job_mode']]),df.copy()])
+
+    return list_of_all_partitions
+
+    '''
+        grouped_df = data_frame.groupby(by=pd.Grouper(key='created_at', freq='D'))
 
         separated_dfs = []
         for date, group in grouped_df:
@@ -40,9 +54,9 @@ def convert_to_partitions(s3_objects):
         for i, day_df in enumerate(separated_dfs, start=1):
             logger.info(f"DataFrame for Day {i}:")
             logger.info(str(day_df))
-            logger.info()
+            logger.info()'''
 
-'''
+    '''
         last_day = -1
         new_dfs = []
         index = -1
@@ -65,11 +79,20 @@ def convert_to_partitions(s3_objects):
 
     return all_dfs'''
 
+
 def check_for():
     pass
 
-def upload_days_to_s3(all_dfs):
+def upload_partitions_to_s3(list_of_all_partitions):
+    logger.info(f"_______ UPLOADING ALL PARTITIONS TO {env['table_name']} _______")
 
+    for date,df in list_of_all_partitions:
+        parquet_bytes = BytesIO(df.to_parquet())
+
+        s3.upload_fileobj(Fileobj=parquet_bytes,Bucket=env['bucket'], Key = f"{env['path']}/{env['table_name']}/company=Locaweb/new-folder/{env['table_name']}_{parameter_dict[env['job_mode']]}={date}/{env['table_name']}.parquet")
+
+
+    '''
     key = f"specialized/incidents/company=Locaweb/{env['path']}test_folder/"
     s3.put_object(Bucket=env['bucket'], Key=key)
 
@@ -80,7 +103,7 @@ def upload_days_to_s3(all_dfs):
         for day in month[1]:
             parquet_bytes = BytesIO(day[1].to_parquet())
 
-            s3.upload_fileobj(Fileobj=parquet_bytes, Bucket=env['bucket'], Key=f"{key}month={month[0]}/day={day[0]}.parquet")
+            s3.upload_fileobj(Fileobj=parquet_bytes, Bucket=env['bucket'], Key=f"{key}month={month[0]}/day={day[0]}.parquet")'''
 
 
 
@@ -98,11 +121,17 @@ logger.info(f"***INITIALIZING GLUEJOB SCRIPT***     Parameters: Bucket = {env['b
 
 s3 = boto3.client('s3')
 
+
+parameter_dict = {
+    "Shrink": 'year',
+    "Expand": 'month'
+}
+
 objects = read_all_objects_in_path()
 
 
-converted_dfs = convert_to_partitions(objects)
-#upload_days_to_s3(converted_dfs)
+partitioned_dfs = convert_to_partitions(objects)
+upload_partitions_to_s3(partitioned_dfs)
 
 
 
